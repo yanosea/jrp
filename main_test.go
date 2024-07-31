3bbe724bd8e398dabc99b496286a22e0c4b192a0
yanosea <myanoshi0626@gmail.com>
2024-07-31 20:49:52 +0900

    wip

--- main_test.go
+++ main_test.go
@@ -13,57 +13,45 @@ import (
 func TestMain(t *testing.T) {
 	tu := usermanager.OSUserProvider{}
 	tcu, _ := tu.Current()
+	dbFileDirPath := filepath.Join(tcu.HomeDir, ".local", "share", "jrp")
+	os.RemoveAll(dbFileDirPath)
 
-	type args struct {
-		command         string
-		commandLineArgs []string
-	}
 	type want struct {
 		exitCode int
 		stdOut   string
 		errOut   string
 	}
 	tests := []struct {
-		name  string
-		args  args
-		want  want
-		setup func(tt *args)
+		name string
+		want want
 	}{
 		{
-			name: "positive testing (no command, no command line args, not downloaded the db file yet)",
-			args: args{command: "", commandLineArgs: []string{""}},
+			name: "positive testing",
 			want: want{exitCode: 0, stdOut: constant.GENERATE_MESSAGE_NOTIFY_DOWNLOAD_REQUIRED + "\n", errOut: ""},
-			setup: func(tt *args) {
-				dbFileDirPath := filepath.Join(tcu.HomeDir, ".local", "share", "jrp")
-				os.RemoveAll(dbFileDirPath)
-			},
 		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			if tt.setup != nil {
-				tt.setup(&tt.args)
-			}
-
 			origOsExit := osExit
 			osExit = func(code int) {
 				if code != tt.want.exitCode {
 					t.Fatalf("osExit was called with code %v", code)
-					t.Errorf("main() exit code = %v, want = %v", code, tt.want.exitCode)
+					t.Errorf("main() : exit code = %v, want = %v", code, tt.want.exitCode)
 				}
 			}
 			defer func() {
 				osExit = origOsExit
 			}()
 
-			cla := append([]string{tt.args.command}, tt.args.commandLineArgs...)
-			os.Args = cla
-			stdOut, errOut := CaptureOutput(t, main)
+			stdOut, errOut := CaptureOutput(t, func() {
+				main()
+			})
+
 			if stdOut != tt.want.stdOut {
-				t.Errorf("main() output '%v', want '%v'", stdOut, tt.want.stdOut)
+				t.Errorf("main() :\n output = '%v', want = '%v'", stdOut, tt.want.stdOut)
 			}
 			if errOut != tt.want.errOut {
-				t.Errorf("main() error output '%v', want '%v'", errOut, tt.want.errOut)
+				t.Errorf("main() :\n error output = '%v', want = '%v'", errOut, tt.want.errOut)
 			}
 		})
 	}
